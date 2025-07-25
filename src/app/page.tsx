e086a69fdb0bfe62b9b5237c4fb27e7c5a937da0
jacquelineleone <jacquelineleone02@gmail.com>
2025-07-25 18:56:27 -0300

    styles: responsive for mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Maintenance } from "@/features/Home/Maintenance";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen overflow-hidden">
       <Maintenance />
     </div>
   );
--- src/features/Home/Maintenance.tsx
+++ src/features/Home/Maintenance.tsx
@@ -3,41 +3,36 @@ import styles from "@/styles/maintenance.module.css";
 
 export const Maintenance = () => {
   return (
-    <section className="w-full h-full flex items-center justify-center lg:justify-start gap-52">
+    <section className="w-full h-full flex flex-col lg:flex-row items-center justify-between lg:justify-start gap-52">
       <Image
         src="/rectangle.svg"
         alt="Imagen decorativa"
-         width={0}
+        width={0}
         height={0}
-        className="hidden lg:block"
-        style={{
-          height: "100%",
-          width: "auto",
-        }}
+        className="hidden lg:block w-auto h-[100%]"
       />
 
       <div className={styles.container}>
-        <Image
-          src="/company.png"
-          alt="Nombre Compañía: Biemes"
-          width={513}
-          height={128}
-        />
+        <div className="relative w-[300px] h-[75px] lg:w-[500px] lg:h-[125px] xl:w-[525px] xl:h-[128px]">
+          <Image src="/company.png" alt="Nombre Compañía: Biemes" fill />
+        </div>
 
         <div className={styles.content}>
-          <Image
-            src="/maintenance.svg"
-            alt="Ícono sitio en construcción"
-            width={238}
-            height={224}
-            id="maintenance-icon"
-          />
+          <div className="relative w-[15vw] h-[10vh] lg:w-[150px] lg:h-[150px] xl:w-[238px] xl:h-[224px]">
+            <Image
+              src="/maintenance.svg"
+              alt="Ícono sitio en construcción"
+              fill
+            />
+          </div>
 
           <div className={styles.text}>
-            <h1 className="text-2xl text-[#1a2b4a] lg:text-4xl">¡Sitio en construcción!</h1>
+            <h1 className="text-2xl text-[#1a2b4a] lg:text-3xl 2xl:text-4xl">
+              ¡Sitio en construcción!
+            </h1>
 
             <div className={styles.description}>
-              <p className="text-1xl text-[#1a2b4a] lg:text-3xl">
+              <p className="text-1xl text-[#1a2b4a] lg:text-2xl 2xl:text-3xl">
                 Estamos construyendo nuestro sitio web para ofrecerte lo mejor.
                 Mientras tanto, podés encontrarnos en Instagram:
               </p>
@@ -50,17 +45,27 @@ export const Maintenance = () => {
                 width={39}
                 height={39}
               />
-              <h2 className="text-1xl text-[#1a2b4a] lg:text-3xl">Biemes.ingenieria</h2>
+              <h2 className="text-1xl text-[#1a2b4a] lg:text-2xl 2xl:text-4xl">
+                Biemes.ingenieria
+              </h2>
             </div>
 
             <div className={styles.thanks}>
-              <h3 className="text-1xl text-[#1a2b4a] lg:text-4xl">
+              <h3 className="text-1xl text-[#1a2b4a] lg:text-2xl 2xl:text-4xl">
                 ¡Gracias por tu paciencia!
               </h3>
             </div>
           </div>
         </div>
       </div>
+
+      <Image
+        src="/rectangle-mobile.svg"
+        alt="Imagen decorativa"
+        width={0}
+        height={0}
+        className="block lg:hidden w-[100vw] h-auto max-h-[300px] object-cover"
+      />
     </section>
   );
 };
